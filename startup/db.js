ffc6d246987a5b96a3b8052a5e272d0fe8247a6b
GOI17 <chivas-gilbert@hotmail.com>
2019-08-07 22:08:19 -0700

    fix connection issue

--- startup/db.js
+++ startup/db.js
@@ -2,10 +2,16 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
-module.exports = function () {
-    const db = process.env.fourseason_db || config.get('db');
-    mongoose.connect(db, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    }).then(() => winston.info(`Connected to ${db}...`));
+module.exports = async () => {
+    const local_db = config.get('local_db');
+    const cloud_db = config.get('cloud_db');
+
+    await mongoose.connect(local_db, { useNewUrlParser: true, useCreateIndex: true })
+        .then(() => winston.info(`Connected to ${local_db}...`))
+        .catch(async () => {
+            winston.info(`Can not connect to ${local_db}, trying to connect to the ${cloud_db}...`)
+            await mongoose.connect(cloud_db, { useNewUrlParser: true, useCreateIndex: true })
+                .then(() => winston.info(`Connected to ${cloud_db}...`))
+                .catch(() => winston.info('Something wrongs with the dbs connection...'));
+        })
 }
