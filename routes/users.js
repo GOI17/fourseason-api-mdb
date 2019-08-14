0723215f7d45fb40cc0e86aa162c639e576036f8
GOI17 <chivas-gilbert@hotmail.com>
2019-08-14 16:55:03 -0700

    fix admin users

--- routes/users.js
+++ routes/users.js
@@ -6,30 +6,32 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/me", auth, async (req, res) => {
-    const user = await User.findById(req.user._id).select("-password");
-    res.send(user);
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
 });
 
 router.post("/", async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send("User already registered.");
-  
-    user = new User(_.pick(req.body, ["firstName", "lastName", "email", "password"]));
-    const salt = bcrypt.genSaltSync(10);
-    const hashPassword = bcrypt.hashSync(user.password, salt);
-    user.password = hashPassword;
-  
-    await user.save();
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
 
-    const token = user.generateAuthToken();
-    res
-        .header("x-auth-token", token)
-        .header("access-control-expose-headers", "x-auth-token")
-        .send(_.pick(user, ["_id", "firstName", "lastName", "email"]));
-    console.log("Hello", user)
+  user = new User(
+    _.pick(req.body, ["firstName", "lastName", "email", "password", "isAdmin"])
+  );
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(user.password, salt);
+  user.password = hashPassword;
+
+  await user.save();
+
+  const token = user.generateAuthToken();
+  res
+    .header("x-auth-token", token)
+    .header("access-control-expose-headers", "x-auth-token")
+    .send(_.pick(user, ["_id", "firstName", "lastName", "email"]));
+  console.log("Hello", user);
 });
 
 module.exports = router;
